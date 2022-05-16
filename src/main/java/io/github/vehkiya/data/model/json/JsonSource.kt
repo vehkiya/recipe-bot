f38d9ef300b5ba41ce09b22b85bcc93ae6aa849b
Sergiu <l33tbird@gmail.com>
2022-05-16 23:01:31 +0300

    Migrate JsonDataProvider to Kotlin

---
+++ src/main/java/io/github/vehkiya/data/model/json/JsonSource.kt
@@ -0,0 +1,12 @@
+package io.github.vehkiya.data.model.json
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+data class JsonSource(
+    val items: Map<String, JsonItem>
+) {
+
+}
---
+++ src/main/java/io/github/vehkiya/service/provider/JsonDataProvider.kt
@@ -0,0 +1,64 @@
+package io.github.vehkiya.service.provider
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.github.vehkiya.config.ServiceProviderProperties
+import io.github.vehkiya.data.model.domain.Item
+import io.github.vehkiya.data.model.json.JsonItem
+import io.github.vehkiya.data.model.json.JsonSource
+import io.github.vehkiya.service.DataProvider
+import org.springframework.beans.factory.annotation.Autowired
+import java.nio.file.Files
+import javax.annotation.PostConstruct
+import kotlin.io.path.Path
+import kotlin.io.path.notExists
+
+class JsonDataProvider : DataProvider {
+
+    private val itemsCache: MutableMap<String, Item> = mutableMapOf()
+
+    lateinit var objectMapper: ObjectMapper
+    lateinit var properties: ServiceProviderProperties
+
+    @Autowired
+    fun initBeans(
+        objectMapper: ObjectMapper,
+        properties: ServiceProviderProperties
+    ) {
+        objectMapper.registerKotlinModule()
+        this.objectMapper = objectMapper
+        this.properties = properties
+    }
+
+    @PostConstruct
+    fun postConstruct() {
+        refresh()
+    }
+
+    override fun itemsCache(): Map<String, Item> {
+        return itemsCache
+    }
+
+    override fun refresh() {
+        itemsCache.clear()
+        val jsonSource = readJsonFile()
+        jsonSource.items
+            .values
+            .stream()
+            .filter { it != null }
+            .filter { it.name != null }
+            .map(convertItem())
+            .forEach { itemsCache[it.name] = it }
+    }
+
+    private fun convertItem() = { jsonItem: JsonItem -> Item(jsonItem.name) }
+
+    private fun readJsonFile(): JsonSource {
+        val path = Path(properties.source)
+        if (path.notExists()) {
+            throw IllegalArgumentException("Source file $path not found")
+        }
+        val content = Files.readAllBytes(path)
+        return objectMapper.readValue(content, JsonSource::class.java)
+    }
+}
