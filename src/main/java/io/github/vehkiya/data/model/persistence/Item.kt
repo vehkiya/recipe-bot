497c94bdb32a2c29dc6a2a39a12ca30352f66f1a
Sergiu Ababii <l33tbird@gmail.com>
2022-11-26 20:50:40 +0200

    Some compatibility fixes for Spring 6

--- src/main/java/io/github/vehkiya/data/model/persistence/Item.kt
+++ src/main/java/io/github/vehkiya/data/model/persistence/Item.kt
@@ -1,11 +1,12 @@
 package io.github.vehkiya.data.model.persistence
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.Table
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
-@Table(name = "Item") data class Item(
+@Table(name = "Item")
+data class Item(
     var name: String, @Id @GeneratedValue var id: Long? = null
 )
